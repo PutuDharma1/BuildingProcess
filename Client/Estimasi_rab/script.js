fbd8ca3e7e5f19fcce064848a3dfac325c7e57d5
PutuDharma <iputudharma.puspa@gmail.com>
2025-07-11 14:39:35 +0700

    update script

--- Client/Estimasi_rab/script.js
+++ Client/Estimasi_rab/script.js
@@ -261,7 +261,7 @@ const populateFormWithHistory = (data) => {
 };
 
 async function handleFormSubmit() {
-    const PYTHON_API_BASE_URL = "https://bnm-application.onrender.com";
+    const PYTHON_API_BASE_URL = "https://buildingprocess-fld9.onrender.com";
     const requiredFields = ['Lokasi', 'Proyek', 'Cabang', 'Lingkup_Pekerjaan'];
     for (const fieldName of requiredFields) {
         const element = form.elements[fieldName];
