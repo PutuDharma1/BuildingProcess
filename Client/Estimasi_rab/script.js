ed2a08d536cb56a9f645ff58f32980c5f25595ab
PutuDharma <iputudharma.puspa@gmail.com>
2025-07-14 10:49:29 +0700

    1222

--- Client/Estimasi_rab/script.js
+++ Client/Estimasi_rab/script.js
@@ -242,7 +242,7 @@ const populateFormWithHistory = (data) => {
 };
 
 async function handleFormSubmit() {
-    const PYTHON_API_BASE_URL = "https://bnm-application.onrender.com";
+    const PYTHON_API_BASE_URL = "https://buildingprocess-fld9.onrender.com";
     
     const requiredFields = ['Lokasi', 'Proyek', 'Cabang', 'Lingkup_Pekerjaan'];
     for (const fieldName of requiredFields) {
@@ -348,7 +348,7 @@ async function initializePage() {
     sipilCategories.forEach(category => sipilTablesWrapper.appendChild(createTableStructure(category, "Sipil")));
     meCategories.forEach(category => meTablesWrapper.appendChild(createTableStructure(category, "ME")));
     
-    const PYTHON_API_BASE_URL = "https://bnm-application.onrender.com";
+    const PYTHON_API_BASE_URL = "https://buildingprocess-fld9.onrender.com";
     const userEmail = sessionStorage.getItem('loggedInUserEmail');
 
     if (userEmail) {
