3d5ec62fd9015df35d1e1725cac753c84f2b2013
PutuDharma <iputudharma.puspa@gmail.com>
2025-07-17 14:48:47 +0700

    Fix: Add apt-get clean to build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# Script ini akan dijalankan oleh Render saat proses build
-
 # Hentikan script jika ada perintah yang gagal
 set -o errexit
 
-# 1. Instal semua dependensi Python dari requirements.txt
+# 1. Instal semua dependensi Python
 pip install -r requirements.txt
 
-# 2. Instal dependensi sistem yang dibutuhkan oleh WeasyPrint
-# Perintah ini menggunakan manajer paket apt-get (untuk sistem Debian/Ubuntu seperti Render)
-apt-get update && apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libcairo2 libgdk-pixbuf-2.0-0
+# 2. Instal dependensi sistem untuk WeasyPrint
+# Membersihkan cache apt terlebih dahulu untuk menghindari error
+apt-get clean
+apt-get update
+apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libcairo2 libgdk-pixbuf-2.0-0
