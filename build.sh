7231c1c2e7235fb3d0a3c1919db8b9b26f1215a8
PutuDharma <iputudharma.puspa@gmail.com>
2025-07-17 14:52:28 +0700

    Final fix: Run apt-get with sudo

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ set -o errexit
 # 1. Instal semua dependensi Python
 pip install -r requirements.txt
 
-# 2. Instal dependensi sistem untuk WeasyPrint
-# Membersihkan cache apt terlebih dahulu untuk menghindari error
-apt-get clean
-apt-get update
-apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libcairo2 libgdk-pixbuf-2.0-0
+# 2. Instal dependensi sistem dengan izin superuser (sudo)
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libcairo2 libgdk-pixbuf-2.0-0
